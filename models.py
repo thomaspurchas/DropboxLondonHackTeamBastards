5aee6bd1235225523a57a163c328593d5297bb17
Thomas Purchas <tpurchas@gmail.com>
2015-01-24 10:02:41 +0000

    Basic user model.
    Probably won't use this

--- models.py
+++ models.py
@@ -1,1 +1,7 @@
 from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False, unique=True)
