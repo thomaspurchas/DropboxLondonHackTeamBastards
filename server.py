f08cb336f9f36259f66c2f0b2491442c7be209bc
seanmtracey <seanmtracey@gmail.com>
2015-01-24 09:24:13 +0000

    Figuring out where we're at

--- server.py
+++ server.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
 	return "Hello!"
 
+@app.route('auth'):
+	#Should be 301 Redirect to Dropbox app authentication		
+	return "auth"
+
+
 if __name__ == "__main__":
 	app.run()
